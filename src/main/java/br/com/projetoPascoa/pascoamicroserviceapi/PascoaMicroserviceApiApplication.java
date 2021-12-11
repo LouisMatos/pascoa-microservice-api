35804ce25f7838653d82b6f6fa8181739b336e9b
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2021-12-11 13:10:34 -0300

    [0.0.1] - commit inicial

---
+++ src/main/java/br/com/projetoPascoa/pascoamicroserviceapi/PascoaMicroserviceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoPascoa.pascoamicroserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PascoaMicroserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PascoaMicroserviceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetoPascoa/pascoamicroserviceapi/PascoaMicroserviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoPascoa.pascoamicroserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PascoaMicroserviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
